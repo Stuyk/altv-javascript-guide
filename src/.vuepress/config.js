691645684f324eff1c8bbe481fd4d16398f026b3
Trevor Wessel <stuykgaming@gmail.com>
2020-09-16 10:51:33 -0600

    patch layout

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,12 +2,14 @@ const { enLocale, enMenus } = require('./locales/en');
 const { trLocale, trMenus } = require('./locales/tr');
 
 const googleSearchConsole = 'bRc7ZyO5gVfceHGhFLN1AvtcptSSPl_6SaLIMHde7bQ';
-const googleAdsenseCode = `ca-pub-3314445478682546`;
 const title = 'alt:V Tutorials by Stuyk';
 const desc = 'Tutorials for the alt:V Framework in JavaScript created by Stuyk. Not for FiveM or RAGE:MP.';
 const card = 'https://i.imgur.com/4yB3BJd.png';
 
 const meta = [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'description', content: desc }],
     ['meta', { itemprop: 'name', content: title }],
     ['meta', { itemprop: 'description', content: desc }],
@@ -24,11 +26,12 @@ const meta = [
     ['meta', { name: 'google-site-verification', content: googleSearchConsole }],
     [
         'script',
-        {
-            'data-ad-client': googleAdsenseCode,
-            async: true,
-            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-        }
+        {},
+        `
+        window._mNHandle = window._mNHandle || {};
+        window._mNHandle.queue = window._mNHandle.queue || [];
+        medianet_versionId = "3121199";
+    `
     ]
 ];
 
@@ -41,12 +44,7 @@ module.exports = {
         // Import all normal locales here.
         ...trLocale
     },
-    head: [
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ...meta
-    ],
+    head: meta,
     themeConfig: {
         repo: 'stuyk/altv-javascript-guide',
         editLinks: false,
--- src/.vuepress/theme/components/Page.vue
+++ src/.vuepress/theme/components/Page.vue
@@ -1,10 +1,23 @@
 <template>
     <main class="page">
         <slot name="top" />
+
         <Content class="theme-default-content" />
         <PageEdit />
+
         <PageNav v-bind="{ sidebarItems }" />
+
         <slot name="bottom" />
+
+        <div id="421256859">
+            <script type="text/javascript">
+    try {
+        window._mNHandle.queue.push(function() {
+            window._mNDetails.loadTag('421256859', '160x600', '421256859');
+        });
+    } catch (error) {}
+            </script>
+        </div>
     </main>
 </template>
 
@@ -14,14 +27,15 @@ import PageNav from '@theme/components/PageNav.vue';
 
 export default {
     components: { PageEdit, PageNav },
-    props: ['sidebarItems']
+    props: ['sidebarItems'],
 };
 </script>
 
 <style lang="stylus">
-@require '../styles/wrapper.styl'
+@require '../styles/wrapper.styl';
 
-.page
-  padding-bottom 2rem
-  display block
+.page {
+  padding-bottom: 2rem;
+  display: block;
+}
 </style>
